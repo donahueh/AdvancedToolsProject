1400706ba61c590cfb5a3d5452a20c0d1f8c7469
Henry Donahue <donahueh@mssm.org>
2023-03-27 17:52:26 -0400

    Automated push :)

--- Animedotjs/script.js
+++ Animedotjs/script.js
@@ -3,6 +3,11 @@ let t1 = anime.timeline({
   autoplay: false
 });
 
+let t2 = anime.timeline({
+  easing: 'easeOutExpo',
+  autoplay: false
+});
+
 t1
   .add({
     targets: '.text',
@@ -15,11 +20,45 @@ t1
     duration: 750
   })
   .add({
+    targets: '.piechart',
+    translateY: -1000,
+    duration: 1
+  })
+  .add({
+    targets: '.nextbutton',
+    translateX: 300,
+    duration: 1
+  })
+  .add({
     targets: '.opener',
     translateY: -1000,
-    duration: 1000
+    duration: 1500
+  })
+  .add({
+    targets: '.piechart',
+    easing: 'easeOutBounce',
+    translateY: 100,
+    duration: 2000
+  })
+  .add({
+    targets: '.nextbutton',
+    easing: 'easeOutElastic',
+    translateX: 0,
+    duration: 2000
   });
 
-  document.querySelector(".button").addEventListener("mouseup",function(){
-    t1.play();
-  })  
+t2
+  .add({
+    targets: '.nextcover',
+    width: '100%',
+    duration: 2000
+  });
+
+
+document.querySelector(".button").addEventListener("mouseup", function () {
+  t1.play();
+})  
+
+document.querySelector(".nextbutton").addEventListener("mouseup", function () {
+  t2.play();
+})  
